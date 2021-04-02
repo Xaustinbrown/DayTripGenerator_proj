2a1be2dbd8cadf16d91cc2d968c6910987a80473
unknown <Xavieraustin1990@gmail.com>
2021-04-02 15:57:19 -0500

    init

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -8,3 +8,27 @@ let randomRestuarants = ['Roscoes Ribs', 'Bobs Burgers', 'Paulas Pastries', 'Pac
 let desireDiningDestination = randomRestuarants[Math.floor((Math.random() * 4) + 0)];
 console.log(desireDiningDestination);
 
+let transitTravel = ['Trains', 'Buses', 'Car', 'Bike'];
+let timelyTransportation = transitTravel[Math.floor((Math.random() * 4) + 0)];
+console.log(timelyTransportation);
+
+let passtimePleasures = ['Kayaking', 'Surfing', 'Hiking', 'Bowling'];
+let plannedEntertainment = passtimePleasures[Math.floor((Math.random() * 4) + 0)];
+console.log(plannedEntertainment);
+
+// let randomReselection = ['selectedDestination', 'desireDiningDestination', 'timelyTransportation', 'plannedEntertainment'];
+// console.log(randomReselection);{
+    
+// }
+
+let expectationsFullfilled = false;
+while(expectationsFullfilled = false){
+    let expectationsFullfilled = prompt("Requested needs to accomodate");
+
+}
+
+Switch(expectationEvaluation);{
+    case"1":
+        console.log(selectedDestination);
+        break;
+}
