bf5bfd6336b88cbcacc5f36988881fa4b419a2dc
unknown <Xavieraustin1990@gmail.com>
2021-04-02 14:22:19 -0500

    init

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -3,3 +3,8 @@
 let planeFlight = ['Japan', 'Germany', 'Costa Rica', 'Belize'];
 let selectedDestination = planeFlight[Math.floor((Math.random() * 4) + 0)];
 console.log(selectedDestination);
+
+let randomRestuarants = ['Roscoes Ribs', 'Bobs Burgers', 'Paulas Pastries', 'Pacific Petes'];
+let desireDiningDestination = randomRestuarants[Math.floor((Math.random() * 4) + 0)];
+console.log(desireDiningDestination);
+
