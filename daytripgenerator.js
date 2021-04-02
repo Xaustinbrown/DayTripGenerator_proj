5bc1454fda1705f8536d3bc6a4cc0ab6da2dbbd1
unknown <Xavieraustin1990@gmail.com>
2021-04-02 13:48:39 -0500

    init

---
+++ daytripgenerator.js
@@ -0,0 +1,5 @@
+"use strict"
+
+let planeFlight = ['Japan', 'Germany', 'Costa Rica', 'Belize'];
+let selectedDestination = planeFlight[Math.floor((Math.random() * 4) + 0)];
+console.log(selectedDestination);
